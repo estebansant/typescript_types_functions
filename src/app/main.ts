1dad1aeb3db70210e2a634bf110d52d7bcff11ee
estebansant <estebansant30@gmail.com>
2023-02-23 10:24:06 -0300

    class 12: Extending interfaces in TS

--- src/app/main.ts
+++ src/app/main.ts
@@ -4,9 +4,12 @@ addProduct({
   id: 1,
   title: 'p1',
   createdAt: new Date(),
+  updatedAt: new Date(),
   stock: 75,
   category: {
     id: 13,
     name: 'c1',
+    createdAt: new Date(),
+    updatedAt: new Date(),
   }
 });
