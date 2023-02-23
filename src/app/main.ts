55a5573eb87bb4284d9fa1fb8bde8960770cbf4d
estebansant <estebansant30@gmail.com>
2023-02-23 11:42:56 -0300

    Learning about omit and pick utility types in TS

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,7 +3,6 @@ import { addProduct, products }  from "./products/product.services";
 
 for(let i = 0; i < 50; i++){
   addProduct({
-    id: faker.datatype.uuid(),
     title: faker.commerce.productName(),
     description: faker.commerce.productDescription(),
     image: faker.image.imageUrl(),
@@ -12,15 +11,8 @@ for(let i = 0; i < 50; i++){
     isNew: faker.datatype.boolean(),
     tags: faker.helpers.arrayElements(),
     size: faker.helpers.arrayElement(['S', 'M', 'L', 'XL']),
-    createdAt: faker.date.recent(),
-    updatedAt: faker.date.recent(),
     stock: faker.datatype.number({min: 10, max: 100}),
-    category: {
-      id: faker.datatype.uuid(),
-      name: faker.commerce.department(),
-      createdAt: faker.date.recent(),
-      updatedAt: faker.date.recent(),
-    }
+    categoryId: faker.datatype.uuid(),
   });
 };
 
